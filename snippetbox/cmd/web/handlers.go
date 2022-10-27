b72c72685d6caa399d32cdda3f7ff73e16d5c869
jacksonwen <jacksonwen@gmail.com>
2022-10-27 14:49:17 +0800

    project struct and organization

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -34,21 +33,4 @@ func create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method Not Allow", http.StatusMethodNotAllowed)
 	}
 	w.Write([]byte("create"))
-}
-
-func main() {
-	// 路由的作用
-	mux := http.NewServeMux()
-	// 注册路由 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", view)
-	mux.HandleFunc("/snippet/create", create)
-
-	log.Print("Starting server in 4000....")
-	// 启动服务器
-	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		// 这个函数的作用是输出错误，并且直接结束程序退出
-		log.Fatal(err)
-	}
 }
---
+++ snippetbox/cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func main() {
+	// 路由的作用
+	mux := http.NewServeMux()
+	// 注册路由 
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", view)
+	mux.HandleFunc("/snippet/create", create)
+
+	log.Print("Starting server in 4000....")
+	// 启动服务器
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		// 这个函数的作用是输出错误，并且直接结束程序退出
+		log.Fatal(err)
+	}
+}
