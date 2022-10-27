d1ccf51a5d333e84405e7a4db8e659bc5351dc69
jacksonwen <jacksonwen@gmail.com>
2022-10-27 14:37:55 +0800

    write to header / http constant / http error

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -6,6 +6,10 @@ import (
 )
 // 处理函数，用于接收请求和发送结果的函数
 func home(w http.ResponseWriter, req *http.Request) {
+	if (req.URL.Path != "/") {
+		http.NotFound(w, req)
+		return 
+	}
 	w.Write([]byte("Hello"))
 }
 
@@ -14,6 +18,12 @@ func view(w http.ResponseWriter, r *http.Request) {
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
+	// http 的常量 ： MethodPost 
+	if (r.Method != http.MethodPost) {
+		w.Header().Set("Allow", "POST")
+		// 可以直接返回错误
+		http.Error(w, "Method Not Allow", 405)
+	}
 	w.Write([]byte("create"))
 }
 
