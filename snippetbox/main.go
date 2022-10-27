14269bedbf72dc47ca04c2d55f3897decfb43761
jacksonwen <jacksonwen@gmail.com>
2022-10-27 14:27:46 +0800

    mvc project / create server and add route

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+// 处理函数，用于接收请求和发送结果的函数
+func home(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello"))
+}
 
+func view(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("view"))
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create"))
+}
 
 func main() {
-	fmt.Println("Hello Golang")
+	// 路由的作用
+	mux := http.NewServeMux()
+	// 注册路由 
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", view)
+	mux.HandleFunc("/snippet/create", create)
+
+	log.Print("Starting server in 4000....")
+	// 启动服务器
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		// 这个函数的作用是输出错误，并且直接结束程序退出
+		log.Fatal(err)
+	}
 }
